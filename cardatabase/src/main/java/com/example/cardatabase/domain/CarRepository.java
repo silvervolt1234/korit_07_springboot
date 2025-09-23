fb15e17cd7a7252b443c0d812a6a404ea61a98c6
silvervolt1234 <silvervolt1234@naver.com>
2025-09-23 11:04:53 +0900

    feat: h2 backup

--- cardatabase/src/main/java/com/example/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/CarRepository.java
@@ -1,4 +1,6 @@
 package com.example.cardatabase.domain;
 
-public class CarRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
 }
