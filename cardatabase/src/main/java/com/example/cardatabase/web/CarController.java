84d622da38b9c6ababd7872355100480ae44b155
silvervolt1234 <silvervolt1234@naver.com>
2025-09-24 11:10:24 +0900

    feat: RESTful backup

--- cardatabase/src/main/java/com/example/cardatabase/web/CarController.java
+++ cardatabase/src/main/java/com/example/cardatabase/web/CarController.java
@@ -1,21 +1,21 @@
-package com.example.cardatabase.web;
-
-import com.example.cardatabase.domain.Car;
-import com.example.cardatabase.domain.CarRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CarController {
-    private final CarRepository carRepository;
-
-    public CarController(CarRepository carRepository) {
-        this.carRepository = carRepository;
-    }
-
-    @GetMapping("/cars")
-    public Iterable<Car> getCars() {
-        // 자동차들이 저장된 테이블에서 전체 명단 추출
-        return carRepository.findAll();
-    }
-}
+//package com.example.cardatabase.web;
+//
+//import com.example.cardatabase.domain.Car;
+//import com.example.cardatabase.domain.CarRepository;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class CarController {
+//    private final CarRepository carRepository;
+//
+//    public CarController(CarRepository carRepository) {
+//        this.carRepository = carRepository;
+//    }
+//
+//    @GetMapping("/cars")
+//    public Iterable<Car> getCars() {
+//        // 자동차들이 저장된 테이블에서 전체 명단 추출
+//        return carRepository.findAll();
+//    }
+//}
