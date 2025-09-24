b2918f28eef314b7e06d7f2585899a6291d24682
silvervolt1234 <silvervolt1234@naver.com>
2025-09-24 17:46:01 +0900

    feat: 20250924 Bean / JWT

--- cardatabase_2/src/main/java/com/example/cardatabase/CardatabaseApplication.java
+++ cardatabase_2/src/main/java/com/example/cardatabase/CardatabaseApplication.java
@@ -1,9 +1,6 @@
 package com.example.cardatabase;
 
-import com.example.cardatabase.domain.Car;
-import com.example.cardatabase.domain.CarRepository;
-import com.example.cardatabase.domain.Owner;
-import com.example.cardatabase.domain.OwnerRepository;
+import com.example.cardatabase.domain.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -20,10 +17,12 @@ public class CardatabaseApplication implements CommandLineRunner {
 
 	private final CarRepository repository;
 	private final OwnerRepository ownerRepository;
+	private final AppUserRepository userRepository;
 
-    public CardatabaseApplication(CarRepository repository, OwnerRepository ownerRepository) {
+	public CardatabaseApplication(CarRepository repository, OwnerRepository ownerRepository, AppUserRepository userRepository) {
         this.repository = repository;
 		this.ownerRepository = ownerRepository;
+        this.userRepository = userRepository;
     }
 
     public static void main(String[] args) {
@@ -46,5 +45,10 @@ public class CardatabaseApplication implements CommandLineRunner {
 		for (Car car : repository.findAll()) {
 			logger.info("brand : {}, model : {}", car.getBrand(), car.getModel());
 		}
+		
+		// AppUser 더미 데이터 추가
+		// 위의 Owner의 경우 owner1 / owner2 만들고 ownerRepository에 저장
+		userRepository.save(new AppUser("user", "$2a$12$uQREMDkMC/AbhWCextHeQ.zJn90ux9kZXrXv1cPr78uZkHYFVz8gu", "USER"));
+		userRepository.save(new AppUser("admin", "$2a$12$7OSM3F78plNmkVb.k1/bN.VJPWsVN5395iopjV7bl5896xtwfHcoS", "ADMIN"));
 	}
 }
